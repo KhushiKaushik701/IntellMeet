c285793bbdaf57c87055e8b152d6b8cc441d8b95
Khushi Kaushik <khushikaushik701@gmail.com>
2026-06-08 17:35:05 +0530

    Initial MERN setup for IntellMeet

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.get("/", (req, res) => {
+    res.send("IntellMeet Server Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

f47afb47bb6b34eff8b866739d6d7f7ea0aa92b0
Khushi Kaushik <khushikaushik701@gmail.com>
2026-06-18 17:14:09 +0530

    Authentication module completed

--- server/server.js
+++ server/server.js
@@ -1,22 +1,35 @@
+const authRoutes = require("./src/routes/authRoutes");
+
+
 const express = require("express");
+
+
+
 const dotenv = require("dotenv");
 const cors = require("cors");
 const helmet = require("helmet");
+const connectDB = require("./src/config/db");
 
 dotenv.config();
 
+// Connect MongoDB
+connectDB();
+
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use("/api/auth", authRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
-    res.send("IntellMeet Server Running");
+  res.send("IntellMeet Server Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

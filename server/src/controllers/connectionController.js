21d69ca75eeb1be20a48681232a44f004c504f21
Khushi Kaushik <khushikaushik701@gmail.com>
2026-07-07 20:24:25 +0530

    Fix Connection model import

--- server/src/controllers/connectionController.js
+++ server/src/controllers/connectionController.js
@@ -1,4 +1,4 @@
-const Connection = require("../models/Connection");
+const Connection = require("../models/connection");
 const Notification = require("../models/Notification");
 
 // Send Connection Request
